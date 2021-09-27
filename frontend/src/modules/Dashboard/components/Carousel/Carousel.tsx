9eddf3021b0f26c86485720610e7b718212e2b06
quantran2381@gmail.com <quantran2381@gmail.com>
2021-09-27 16:33:48 +0700

    change image carousel

--- frontend/src/modules/Dashboard/components/Carousel/Carousel.tsx
+++ frontend/src/modules/Dashboard/components/Carousel/Carousel.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { CarouselBoxStyled } from "./Carousel.styled"
-import custom14 from "@/assets/images/custom-14.svg"
+import custom15 from "@/assets/images/custom-15.svg"
 
 export default function Carousel() {
   return (
@@ -9,14 +9,14 @@ export default function Carousel() {
         <div className="box-body pr-0 pl-lg-50 pl-15 py-0">
           <div className="row align-items-center">
             <div className="col-12 col-lg-8">
-              <h1 className="font-size-40 text-white">Welcome Jhone!</h1>
+              <h1 className="font-size-40 text-white">Welcome to Elearn!</h1>
               <p className="text-white mb-0 font-size-20">
                 Education is the passport to the future, So learn more &amp;
                 more
               </p>
             </div>
             <div className="col-12 col-lg-4">
-              <img src={custom14} alt="true" />
+              <img src={custom15} alt="true" />
             </div>
           </div>
         </div>
