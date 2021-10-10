b01620d8c9d54b57b3d309a122bc70c1465ba3ba
Vinhthai-0601 <80025976+Vinhthai-0601@users.noreply.github.com>
2021-10-10 21:12:57 +0700

    Next-Arrow

---
+++ frontend/src/modules/Dashboard/components/CourseSlider/NextArrow.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const NextArrowStyle = styled.div`
+
+.nextarrow{
+    borderRadius: "50%",
+    top: "40%",
+    right: "-1.6rem",
+    height: "40px",
+    width: "40px",
+    border: "1px solid #6a6f73",
+    boxshadow: "0 2px 4px rgb(0 0 0 / 8%), 0 4px 12px rgb(0 0 0 / 8%)"
+}
+
+`
