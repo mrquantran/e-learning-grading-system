2009eb7ebd86b491bc7f6f39c4a249a24b0cc435
quantran2381@gmail.com <quantran2381@gmail.com>
2021-10-13 16:42:53 +0700

    add error notification fetch courses

--- frontend/src/modules/Dashboard/saga/index.ts
+++ frontend/src/modules/Dashboard/saga/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { API } from "@/apis"
+import { errorNotification, getError } from "@/utils/notification"
 import { all, call, put, takeLatest } from "@redux-saga/core/effects"
 
 import actions from "../action/index"
@@ -9,7 +10,13 @@ function* fetchCoursesData() {
     const { data: courses } = yield call(API.fetchCourses)
 
     yield put({ type: actions.FETCH_COURSES_DATA.SUCCESS, payload: courses })
-  } catch (error) {}
+  } catch (error: any) {
+    errorNotification(getError(error))
+    yield put({
+      type: actions.FETCH_COURSES_DATA.ERROR,
+      payload: error.message
+    })
+  }
 }
 
 function* watchFetchCoursesData() {
