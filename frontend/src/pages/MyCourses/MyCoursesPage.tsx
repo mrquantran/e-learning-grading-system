8100e540b1b908b2ce49504352a59f63a0686780
quantran2381@gmail.com <quantran2381@gmail.com>
2021-10-16 16:39:49 +0700

    tab antd

--- frontend/src/pages/MyCourses/MyCoursesPage.tsx
+++ frontend/src/pages/MyCourses/MyCoursesPage.tsx
@@ -1,6 +1,25 @@
-import MyCourses from "@/modules/Course/Container/MyCourses/MyCourses"
 import React from "react"
+import { Tabs } from "antd"
 
+const { TabPane } = Tabs
+
+function callback(key) {
+  console.log(key)
+}
 export default function MyCoursesPage() {
-  return <MyCourses></MyCourses>
+  return (
+    <div>
+      <Tabs defaultActiveKey="1" onChange={callback}>
+        <TabPane tab="Tab 1" key="1">
+          Content of Tab Pane 1
+        </TabPane>
+        <TabPane tab="Tab 2" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="Tab 3" key="3">
+          Content of Tab Pane 3
+        </TabPane>
+      </Tabs>
+    </div>
+  )
 }
