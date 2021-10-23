7b1edf4199c077975eddb31531ca1ccc5589b1b5
quantran2381@gmail.com <quantran2381@gmail.com>
2021-10-24 01:51:15 +0700

    reducer create draft course

--- frontend/src/modules/Course/reducers/createCourseReducer.ts
+++ frontend/src/modules/Course/reducers/createCourseReducer.ts
@@ -1,12 +1,11 @@
 import { GO_TO_STEP } from "../action/createCourseAction"
-import CategoryCourse from "../Components/Step/CategoryCourse/CategoryCourse"
-import Finish from "../Components/Step/Finish/Finish"
 import TitleCourse, {
   validateSchemaTitleCourse
 } from "../Components/Step/TitleCourse/TitleCourse"
 import TypeCourse, {
   validateSchemaTypeCourse
 } from "../Components/Step/TypeCourse/TypeCourse"
+import actionsCreateCourse from "../action/createCourseAction"
 
 const initialState = {
   isFetching: false,
@@ -45,6 +44,12 @@ const createCourseReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case GO_TO_STEP:
       return { ...state, step: payload }
+    case actionsCreateCourse.CREATE_DRAFT_COURSE.REQUEST:
+      return { ...state, isProcessing: true }
+    case actionsCreateCourse.CREATE_DRAFT_COURSE.SUCCESS:
+      return { ...state, isProcessing: false, step: 1 }
+    case actionsCreateCourse.CREATE_DRAFT_COURSE.ERROR:
+      return { ...state, isProcessing: false, error: payload }
     default:
       return state
   }
